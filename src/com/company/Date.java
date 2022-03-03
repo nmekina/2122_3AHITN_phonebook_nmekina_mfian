a328c8be17eff2f47d6627ecc8d9385409ba54d4
nmekina <95867724+nmekina@users.noreply.github.com>
2022-03-03 14:12:58 +0100

    Person, Date, PhoneNumber

--- src/com/company/Date.java
+++ src/com/company/Date.java
@@ -8,4 +8,28 @@ public class Date {
         this.mon = mon;
         this.year = year;
     }
+
+    public Date(String date) {
+
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMon() {
+        return mon;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    boolean isLeapYear() {
+        return true;
+    }
+
+    boolean isValid(Date d) {
+        return true;
+    }
 }
---
+++ src/com/company/StreamOperation.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.Reader;
+import java.io.Writer;
+
+public class StreamOperation {
+
+    void ToStream(Writer w, Person p) {
+
+    }
+
+    Person fromStream(Reader r) {
+
+        return
+    }
+}
