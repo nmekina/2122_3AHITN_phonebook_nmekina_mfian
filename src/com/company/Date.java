8f2717f73ce429adf5158efbbfa663ea88f5d069
nmekina <95867724+nmekina@users.noreply.github.com>
2022-03-03 13:59:20 +0100

    Person, Date, PhoneNumber

--- src/com/company/Date.java
+++ src/com/company/Date.java
@@ -2,4 +2,10 @@ package com.company;
 
 public class Date {
     int day, mon, year;
+
+    public Date(int day, int mon, int year) {
+        this.day = day;
+        this.mon = mon;
+        this.year = year;
+    }
 }
