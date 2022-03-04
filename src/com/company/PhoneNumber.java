c6289fef3728061c03008b038b065f7b0441e7a3
fianm <mfian@htl-steyr.ac.at>
2022-03-04 08:40:48 +0100

    PhoneNumber.java -> isValid bearbeitet

--- src/com/company/PhoneNumber.java
+++ src/com/company/PhoneNumber.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author nmekina
  * speichert eine Telefonnummer
@@ -29,9 +32,21 @@ public class PhoneNumber {
         return number;
     }
 
-    boolean isValid(PhoneNumber b) {
-        return true;
-    }
+    public static boolean isValid(String s) {
+        Pattern p = Pattern.compile("^\\+(?:[0-9] ?){6,14}[0-9]$");
+        Matcher m = p.matcher(s);
+        return (m.find() && m.group().equals(s));
+
+        String phoneNumbers = country + areacode + number;
+
+        for (int i = 0; i < phoneNumbers.length; i++) {
+            String phoneNumber=phoneNumbers[i];
+            if (isValid(phoneNumber))
+                System.out.print(phoneNumber+" ist telenummer");
+            else
+                System.out.print(phoneNumber+" keine telenummer");
+        }
+
 
     @Override
     public String toString() {
@@ -40,5 +55,6 @@ public class PhoneNumber {
                 ", areacode=" + areacode +
                 ", number=" + number +
                 '}';
+
     }
 }
